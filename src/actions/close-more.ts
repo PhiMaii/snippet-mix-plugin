f9bfb7889bfe6ee8f2d6035d3840d3daec18c152
PhiMaii <pmairguenther@web.de>
2025-07-07 20:30:10 +0200

    Add Close More action with SVG icons and update manifest

--- src/actions/close-more.ts
+++ src/actions/close-more.ts
@@ -7,19 +7,19 @@ import streamDeck, {
 /**
  * An action that logs a Stream Deck key press.
  */
-@action({ UUID: "net.phimai.snippet-mix-plugin.open-more" })
-export class OpenMore extends SingletonAction {
+@action({ UUID: "net.phimai.snippet-mix-plugin.close-more" })
+export class CloseMore extends SingletonAction {
   /**
    * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
    * @param ev Information about the event.
    */
   override onKeyDown(ev: KeyDownEvent<JsonObject>): void | Promise<void> {
-    streamDeck.logger.info("OpenMore button pressed");
+    streamDeck.logger.info("CloseMore button pressed");
 
     streamDeck.profiles.switchToProfile(
       ev.action.device.id,
       "SnippetMix-Default",
-      1
+      0
     );
   }
 }
--- src/plugin.ts
+++ src/plugin.ts
@@ -5,6 +5,7 @@ import { LoadSnippet } from "./actions/load-snippet";
 import { RowDown } from "./actions/row-down";
 import { RowUp } from "./actions/row-up";
 import { OpenMore } from "./actions/open-more";
+import { CloseMore } from "./actions/close-more";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.INFO);
@@ -15,6 +16,7 @@ streamDeck.actions.registerAction(new LoadSnippet());
 streamDeck.actions.registerAction(new RowDown());
 streamDeck.actions.registerAction(new RowUp());
 streamDeck.actions.registerAction(new OpenMore());
+streamDeck.actions.registerAction(new CloseMore());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
