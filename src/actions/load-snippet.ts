99e96e5a5bfadb11685d0983b6e7e9255b3d6663
PhiMaii <pmairguenther@web.de>
2025-07-07 20:08:27 +0200

    Refactor LoadSnippet class to enhance key event handling and logging

--- src/actions/load-snippet.ts
+++ src/actions/load-snippet.ts
@@ -1,14 +1,60 @@
-import streamDeck, { action, type JsonObject, type KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+import streamDeck, {
+  action,
+  type JsonObject,
+  type KeyDownEvent,
+  KeyUpEvent,
+  SingletonAction,
+  WillAppearEvent,
+} from "@elgato/streamdeck";
 /**
  * An action that logs a Stream Deck key press.
  */
 @action({ UUID: "net.phimai.snippet-mix-plugin.load-snippet" })
-export class LoadSnippet extends SingletonAction {
-	/**
-	 * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
-	 * @param ev Information about the event.
-	 */
-	override onKeyDown(ev: KeyDownEvent<JsonObject>): void | Promise<void> {
-		streamDeck.logger.info("LoadSnippet button pressed");
-	}
+export class LoadSnippet extends SingletonAction<LoadSnippetSettings> {
+  /**
+   * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
+   * @param ev Information about the event.
+   */
+
+  constructor() {
+    super();
+    streamDeck.logger.info("LoadSnippet : Constructor called");
+  }
+
+  override onWillAppear(
+    ev: WillAppearEvent<LoadSnippetSettings>
+  ): Promise<void> | void {
+    ev.action.setTitle(
+      ev.action.coordinates?.row + " " + ev.action.coordinates?.column
+    );
+  }
+
+  override async onKeyUp(ev: KeyUpEvent<LoadSnippetSettings>): Promise<void> {
+    let currentState = ev.payload.state;
+    if (currentState == 1) {
+      ev.action.setState(0);
+      await ev.action.setSettings({
+        used: true,
+      });
+    } else if (currentState == 0) {
+      ev.action.setState(1);
+      await ev.action.setSettings({
+        used: false,
+      });
+    }
+    streamDeck.logger.info(ev.payload.settings.used);
+  }
+
+  override onKeyDown(
+    ev: KeyDownEvent<LoadSnippetSettings>
+  ): void | Promise<void> {
+    streamDeck.logger.info("LoadSnippet button pressed");
+    streamDeck.logger.info(ev.action.coordinates);
+  }
 }
+
+type LoadSnippetSettings = {
+  used: boolean;
+  id: number;
+  position: [number, number];
+};
