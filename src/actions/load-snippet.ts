b690c02082aa4c0c7be9b711acfb4a332f528f4a
PhiMaii <pmairguenther@web.de>
2025-07-08 09:46:03 +0200

    Added long press functionality

--- src/actions/load-snippet.ts
+++ src/actions/load-snippet.ts
@@ -16,6 +16,8 @@ export class LoadSnippet extends SingletonAction<LoadSnippetSettings> {
    * @param ev Information about the event.
    */
 
+  timestamp: number = 0;
+
   constructor() {
     super();
     streamDeck.logger.info("LoadSnippet : Constructor called");
@@ -26,30 +28,58 @@ export class LoadSnippet extends SingletonAction<LoadSnippetSettings> {
   ): Promise<void> | void {
     ev.action.setTitle(
       ev.action.coordinates?.row + " " + ev.action.coordinates?.column
+      //   ev.payload.settings.used?.toString()
     );
   }
 
   override async onKeyUp(ev: KeyUpEvent<LoadSnippetSettings>): Promise<void> {
-    let currentState = ev.payload.state;
-    if (currentState == 1) {
-      ev.action.setState(0);
-      await ev.action.setSettings({
-        used: true,
-      });
-    } else if (currentState == 0) {
-      ev.action.setState(1);
-      await ev.action.setSettings({
-        used: false,
-      });
+    let duration = Date.now() - this.timestamp;
+    streamDeck.logger.info("Duration: " + duration);
+    if (duration < 500) {
+      // Short Press
+      this.shortPress(ev);
+      // streamDeck.logger.info(ev.payload.settings.used);
+    } else {
+      this.longPress(ev);
+      // Long Press
     }
-    streamDeck.logger.info(ev.payload.settings.used);
   }
 
   override onKeyDown(
     ev: KeyDownEvent<LoadSnippetSettings>
   ): void | Promise<void> {
+    // streamDeck.logger.info("LoadSnippet button pressed");
+    // streamDeck.logger.info(ev.action.coordinates);
+
+    this.timestamp = Date.now();
+  }
+
+  // #####################
+  async shortPress(ev: KeyUpEvent<LoadSnippetSettings>): Promise<void> {
     streamDeck.logger.info("LoadSnippet button pressed");
-    streamDeck.logger.info(ev.action.coordinates);
+    let currentState = ev.payload.state;
+    streamDeck.logger.info("Current state: " + currentState);
+
+    if (ev.payload.settings.used === true) {
+      streamDeck.logger.info("LOAD SNIPPET");
+      return;
+    }
+
+    if (currentState == 0) {
+      ev.action.setState(1);
+      await ev.action.setSettings({
+        used: true,
+      });
+    } else if (currentState == 1) {
+      ev.action.setState(0);
+      await ev.action.setSettings({
+        used: false,
+      });
+    }
+  }
+
+  async longPress(ev: KeyUpEvent<LoadSnippetSettings>): Promise<void> {
+    streamDeck.logger.info("LoadSnippet button long pressed");
   }
 }
 
