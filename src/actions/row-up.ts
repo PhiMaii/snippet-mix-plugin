b9273b7365cf49032a3d290b784aae2106d6c7f7
PhiMaii <pmairguenther@web.de>
2025-07-09 17:26:33 +0200

    Refactor RowUp action to improve code readability and maintainability

--- src/actions/row-up.ts
+++ src/actions/row-up.ts
@@ -1,14 +1,25 @@
-import streamDeck, { action, type JsonObject, type KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+import streamDeck, {
+  action,
+  type JsonObject,
+  type KeyDownEvent,
+  SingletonAction,
+  WillAppearEvent,
+} from "@elgato/streamdeck";
 /**
  * An action that logs a Stream Deck key press.
  */
 @action({ UUID: "net.phimai.snippet-mix-plugin.row-up" })
 export class RowUp extends SingletonAction {
-    /**
-     * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
-     * @param ev Information about the event.
-     */
-    override onKeyDown(ev: KeyDownEvent<JsonObject>): void | Promise<void> {
-        streamDeck.logger.info("RowUp button pressed");
-    }
+  /**
+   * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
+   * @param ev Information about the event.
+   */
+  override onKeyDown(ev: KeyDownEvent<JsonObject>): void | Promise<void> {
+    streamDeck.logger.info("RowUp button pressed");
+  }
+
+  override onWillAppear(ev: WillAppearEvent<JsonObject>): Promise<void> | void {
+    const svg = ``;
+    ev.action.setImage(`data:image/svg+xml,${encodeURIComponent(svg)}`);
+  }
 }
