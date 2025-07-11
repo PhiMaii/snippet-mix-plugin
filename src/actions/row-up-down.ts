c0aadc01760f738e2569ba799600c3599ecf83de
PhiMaii <pmairguenther@web.de>
2025-07-11 14:07:47 +0200

    Add SVG icons for Row Up and Row Down actions

---
+++ src/actions/row-up-down.ts
@@ -0,0 +1,19 @@
+import streamDeck, {
+  action,
+  type JsonObject,
+  type KeyDownEvent,
+  SingletonAction,
+} from "@elgato/streamdeck";
+/**
+ * An action that logs a Stream Deck key press.
+ */
+@action({ UUID: "net.phimai.snippet-mix-plugin.row-up-down" })
+export class RowDown extends SingletonAction {
+  /**
+   * Handles the user pressing a Stream Deck key (pedal, G-key, etc).
+   * @param ev Information about the event.
+   */
+  override onKeyDown(ev: KeyDownEvent<JsonObject>): void | Promise<void> {
+    streamDeck.logger.info("RowDown button pressed");
+  }
+}
