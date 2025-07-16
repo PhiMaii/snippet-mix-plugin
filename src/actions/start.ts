6883aad317cfae038247d2da1ab96ceccf0b0bff
PhiMaii <pmairguenther@web.de>
2025-07-16 20:27:38 +0200

    Added "Start" action with a PropertyInspector that saves settings for ws to global settings

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -1,11 +1,38 @@
-import streamDeck, { action, JsonObject, KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
+import streamDeck, {
+	action,
+	DidReceiveSettingsEvent,
+	JsonObject,
+	KeyDownEvent,
+	SingletonAction,
+	WillAppearEvent,
+} from "@elgato/streamdeck";
 
 @action({ UUID: "net.phimai.snippet-mix-plugin.start" })
 export class Start extends SingletonAction<JsonObject> {
 	override async onKeyDown(ev: KeyDownEvent<JsonObject>): Promise<void> {
-		streamDeck.logger.warn("Starting Snippet-Mix");
+		streamDeck.logger.warn("Starting Snippet-Mix", await streamDeck.settings.getGlobalSettings());
 		// Update the count from the settings.
 
 		streamDeck.profiles.switchToProfile(ev.action.device.id, "SnippetMix-Default", 0);
 	}
+
+	override async onDidReceiveSettings(ev: DidReceiveSettingsEvent<StartSettings>): Promise<void> {
+		let settings = ev.payload.settings;
+
+		await streamDeck.settings.setGlobalSettings({
+			ws: {
+				host: settings.ws.host,
+				port: settings.ws.port,
+				show_name: settings.ws.show_name,
+			},
+		});
+	}
 }
+
+type StartSettings = {
+	ws: {
+		host: string;
+		port: number;
+		show_name: string;
+	};
+};
