13efb48a9d9d0ea5d873d204ddbc44eae1d68443
PhiMaii <pmairguenther@web.de>
2025-07-16 17:57:09 +0200

    Scroll buttons show the row number upon calling WillAppear

--- src/actions/navigate/scroll.ts
+++ src/actions/navigate/scroll.ts
@@ -27,7 +27,7 @@ export class Scroll extends SingletonAction {
 		//Reset scroll_offset to 0 on plugin restart/streamdeck software start
 	}
 
-	override async onKeyDown(ev: KeyDownEvent<JsonObject>) {
+	override async onKeyDown(ev: KeyDownEvent<ScrollSettings>) {
 		streamDeck.logger.info("Scroll button pressed");
 		/*const scrollSettings =
             await streamDeck.settings.getGlobalSettings<GlobalScrollSettings>();*/
@@ -77,11 +77,14 @@ export class Scroll extends SingletonAction {
 		} else {
 			streamDeck.logger.warn("Unknown scroll direction", ev.payload.settings.scroll_direction);
 		}
+
+		RenderScroll(ev.action, ev.payload.settings);
 	}
 
-	override onWillAppear(ev: WillAppearEvent<JsonObject>): Promise<void> | void {
+	override onWillAppear(ev: WillAppearEvent<ScrollSettings>): Promise<void> | void {
 		// const scroll_offset = streamDeck.settings.getGlobalSettings();
 		// streamDeck.logger.info(scroll_offset);
+		RenderScroll(ev.action, ev.payload.settings);
 	}
 
 	override onDidReceiveSettings(ev: DidReceiveSettingsEvent<ScrollSettings>): Promise<void> | void {
@@ -112,6 +115,20 @@ export class Scroll extends SingletonAction {
 
 		return Promise.resolve();
 	}
+}
+
+export async function RenderScroll(action: any, settings?: ScrollSettings) {
+	streamDeck.logger.info("Current Scorll Offset: ", SCROLL_OFFSET);
+	streamDeck.logger.info("Current Scorll Offset: ", settings);
+
+	let svgUp = `<svg viewBox="0 0 512 512"> <path d="M 341.202 187.277 C 349.571 178.908 363.162 178.908 371.53 187.277 L 478.649 294.395 C 487.018 302.764 487.018 316.355 478.649 324.724 C 470.281 333.092 456.69 333.092 448.321 324.724 L 356.333 232.735 L 264.345 324.657 C 255.976 333.025 242.385 333.025 234.017 324.657 C 225.648 316.288 225.648 302.697 234.017 294.329 L 341.135 187.21 L 341.202 187.277 Z" style="stroke-width: 1; fill: rgb(255, 255, 255);"/> <rect width="200" height="512" style="fill: rgb(216, 216, 216);"/> <text style="font-family: Arial, sans-serif; font-size: 130px; font-weight: 700; white-space: pre; stroke-width: 1; text-anchor: middle;" x="104.299" y="300.597">${SCROLL_OFFSET + 2}</text> </svg>`;
+	let svgDown = `<svg viewBox="0 0 512 512" width="512px" height="512px" xmlns:bx="https://boxy-svg.com"> <rect width="200" height="512" style="fill: rgb(216, 216, 216);" transform="matrix(1, 0, 0, 1, 5.684341886080802e-14, 0)"/> <text style="font-family: Arial, sans-serif; font-size: 130px; font-weight: 700; white-space: pre; stroke-width: 1; text-anchor: middle;" x="104.412" y="300.796">${SCROLL_OFFSET + 3}</text> <path d="M 341.162 324.724 C 349.531 333.092 363.122 333.092 371.49 324.724 L 478.609 217.605 C 486.978 209.236 486.978 195.645 478.609 187.277 C 470.241 178.908 456.65 178.908 448.281 187.277 L 356.293 279.265 L 264.305 187.343 C 255.936 178.975 242.345 178.975 233.977 187.343 C 225.608 195.712 225.608 209.303 233.977 217.672 L 341.095 324.79 L 341.162 324.724 Z" style="stroke-width: 1; fill: rgb(255, 255, 255);"/> </svg>`;
+
+	await action.setImage(
+		`data:image/svg+xml,${encodeURIComponent(settings?.scroll_direction === "up" ? svgUp : svgDown)}`,
+	);
+
+	action.setState(settings?.scroll_direction === "up" ? 0 : 1);
 }
 
 type ScrollSettings = {
